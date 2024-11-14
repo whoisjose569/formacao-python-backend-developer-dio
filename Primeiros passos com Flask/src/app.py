3ebbde83cf406fc35f54631af248b12783341843
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-11-14 17:04:15 -0300

    config database connection

--- Primeiros passos com Flask/src/app.py
+++ Primeiros passos com Flask/src/app.py
@@ -1,20 +1,68 @@
-from flask import Flask, url_for, request
+import os
 
-app = Flask(__name__)
+from flask import Flask
+from datetime import datetime
+from flask import Flask, current_app
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
+import sqlalchemy as sa
 
-@app.route("/", methods=["GET", "POST"])
-def hello_world():
-    if request.mtheod == 'GET':
-        return 'Hello, World com GET'
-    return "<p> Hello, World com POST</p>"
+import click
 
-@app.route("/json")
-def json():
-    return {
-        'message': "Olá Mundo"
-    }
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
 
 
-with app.test_request_context():
-    print(url_for("hello_world"))
-    print(url_for("json"))
+class User(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    username: Mapped[str] = mapped_column(sa.String, unique=True)
+    
+    def __repr__(self):
+        return f"Users(id={self.id!r}, username={self.username!r})"
+
+
+class Post(db.Model):
+    id: Mapped[int] = mapped_column(sa.Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(sa.String, nullable=False)
+    body: Mapped[str] = mapped_column(sa.String, nullable=False)
+    created: Mapped[datetime] = mapped_column(sa.DateTime, server_default=sa.func.now())
+    author_id: Mapped[int] = mapped_column(sa.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return f"Users(id={self.id!r}, title={self.title!r}, author_id={self.author_id!r})"
+
+@click.command('init-db')
+def init_db_command():
+    global db
+    with current_app.app_context():
+        db.create_all()
+    click.echo('Initialized the database.')
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev',
+        SQLALCHEMY_DATABASE_URI="sqlite:///dio_bank.sqlite",
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    app.cli.add_command(init_db_command)  
+    db.init_app(app)
+    
+    return app
