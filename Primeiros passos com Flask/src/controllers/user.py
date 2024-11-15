fd72822c272fcee4c89ae6d310ce4d2a4aa063f1
Jose Vitor Araujo <jose.vitor.araujo569@hotmail.com>
2024-11-15 18:38:49 -0300

    implementing get_user and get_user by id

--- Primeiros passos com Flask/src/controllers/user.py
+++ Primeiros passos com Flask/src/controllers/user.py
@@ -11,10 +11,25 @@ def _create_user():
     db.session.add(user)
     db.session.commit()
 
+def _list_users():
+    query = db.select(User)
+    users = db.session.execute(query).scalars()
+    return [
+        {"id": user.id, "username": user.username} for user in users
+    ]
+
 @bp.route('/', methods=["GET", "POST"])
 def handle_users():
     if request.method == 'POST':
         _create_user()
         return {'message': 'User created!'}, HTTPStatus.CREATED
     else:
-        return {"users": []}
+        return {"users": _list_users()}
+
+@bp.route('/<int:user_id>')
+def get_user(user_id):
+    user = db.get_or_404(User, user_id)
+    return {
+        "id": user.id, 
+        "username": user.username
+    }
